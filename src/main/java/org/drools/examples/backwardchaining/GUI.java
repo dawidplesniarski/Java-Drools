5feea4946a1a9289e38543a83316ada05c7b46bf
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-07 19:32:11 +0100

    Some rules added and bugs fixed

--- src/main/java/org/drools/examples/backwardchaining/GUI.java
+++ src/main/java/org/drools/examples/backwardchaining/GUI.java
@@ -1,13 +1,8 @@
 package org.drools.examples.backwardchaining;
 
-import org.kie.api.runtime.KieSession;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.ByteArrayInputStream;
-import java.io.Console;
-import java.util.Scanner;
 
 
 public class GUI extends JFrame implements ActionListener {
@@ -15,7 +10,7 @@ public class GUI extends JFrame implements ActionListener {
     public static JLabel lMessage;
     public static JButton yButton,nButton;
     public static String message="";
-    public static JLabel LAnswer;
+    public static JTextArea lAnswer;
 
     //Scanner scanner = new Scanner(System.in);
 
@@ -24,21 +19,21 @@ public class GUI extends JFrame implements ActionListener {
         setSize(400, 400);
         setLayout(null);
 
-        lMessage = new JLabel("test");
-        lMessage.setBounds(10,10,380,20);
+        lMessage = new JLabel();
+        lMessage.setBounds(10,10,380,60);
         add(lMessage);
 
         yButton = new JButton("Tak");
-        yButton.setBounds(10,40,70,20);
+        yButton.setBounds(10,60,70,20);
         add(yButton);
 
         nButton = new JButton("Nie");
-        nButton.setBounds(320,40,70,20);
+        nButton.setBounds(320,60,70,20);
         add(nButton);
 
-        LAnswer = new JLabel("rozwiązanie");
-        LAnswer.setBounds(10,100,380,30);
-        add(LAnswer);
+        lAnswer = new JTextArea();
+        lAnswer.setBounds(10,100,380,80);
+        add(lAnswer);
 
     }
 
