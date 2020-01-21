937c81ca08715a26ad30bcf2e22add7950e17b59
dawidplesniarski <dawidplesniarski@gmail.com>
2020-01-21 18:29:20 +0100

    Combo box added. Works but it have bugs.

--- src/main/java/org/drools/examples/backwardchaining/GUI.java
+++ src/main/java/org/drools/examples/backwardchaining/GUI.java
@@ -3,6 +3,8 @@ package org.drools.examples.backwardchaining;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 
 public class GUI extends JFrame implements ActionListener {
@@ -11,7 +13,10 @@ public class GUI extends JFrame implements ActionListener {
     public static JButton yButton,nButton;
     public static String message="";
     public static JTextArea lAnswer;
+    public static String selectedIssue="";
+    public static JComboBox lComboBox;
 
+    String[] issues = {"Zasilanie","Obraz","Dźwięk","Aplikacja"};
     //Scanner scanner = new Scanner(System.in);
 
     public GUI(){
@@ -34,6 +39,23 @@ public class GUI extends JFrame implements ActionListener {
         lAnswer = new JTextArea();
         lAnswer.setBounds(10,100,480,80);
         add(lAnswer);
+
+        lComboBox = new JComboBox(issues);
+        lComboBox.setEditable(true);
+        lComboBox.setBounds(10,200,480,40);
+        add(lComboBox);
+
+        lComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent itemEvent) {
+                if(itemEvent.getStateChange() == ItemEvent.SELECTED){
+                    selectedIssue = lComboBox.getSelectedItem().toString();
+                    System.out.println(selectedIssue);
+                }
+            }
+        });
+
+
 
     }
 
